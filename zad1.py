29335d586c19de22a538be93c889351ae9c08fd3
kamczill <kam134@interia.eu>
2020-11-11 18:56:52 +0100

    napisanie funkcji i sprawdzenie dzialania

--- zad1.py
+++ zad1.py
@@ -1,7 +1,22 @@
 #Proszę posortować podaną listę po drugim elemencie każdej listy, a w przypadku, kiedy są równe to po trzecim elemencie:
 
+def sort(list_to_sort):
+    for x in range(len(list_to_sort)):
+        for y in range(x+1,len(list_to_sort)):
+            if list_to_sort[x][1] != list_to_sort[y][1]:
+                if list_to_sort[x][1] > list_to_sort[y][1]:
+                    tmp = list_to_sort[y]
+                    list_to_sort[y] = list_to_sort[x]
+                    list_to_sort[x] = tmp
+            else:
+                if list_to_sort[x][2] > list_to_sort[y][2]:
+                    tmp = list_to_sort[y]
+                    list_to_sort[y] = list_to_sort[x]
+                    list_to_sort[x] = tmp
+
+
 list_to_sort = [[3, 2, 3], [2, 0, 2], [3, 0, 1]]
 
-for x in range(3):
-    for y in range(2):
-        if list_to_sort[x][y+1]
+
+sort(list_to_sort)
+print(list_to_sort)
