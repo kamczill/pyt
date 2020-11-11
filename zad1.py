7f6a62141cb5d970a564979ffaf395e047b014d9
kamczill <kam134@interia.eu>
2020-11-11 15:26:23 +0100

    poczatek

---
+++ zad1.py
@@ -0,0 +1,7 @@
+#Proszę posortować podaną listę po drugim elemencie każdej listy, a w przypadku, kiedy są równe to po trzecim elemencie:
+
+list_to_sort = [[3, 2, 3], [2, 0, 2], [3, 0, 1]]
+
+for x in range(3):
+    for y in range(2):
+        list_to_sort[x][y+1]
