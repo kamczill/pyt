08a28e853ded37dc7bfa2cb96e3a0570e4442bf3
kamczill <kam134@interia.eu>
2020-11-24 15:14:58 +0100

    finalna czesc zadania

--- zad5.py
+++ zad5.py
@@ -1,3 +1,4 @@
+from random import randint
 
 imiona = ['Damian', 'Ola', 'Barbara', 'Robert', 'Zygmunt', 'Ewa']
 imiona2 = []
@@ -6,3 +7,11 @@ for name in imiona:
     imiona2.append(name[0])
 
 print(imiona2)
+
+randomNum = []
+num = 0
+for i in range(5):
+        randomNum2 = [randint(1,10) for el in range(4)]
+        randomNum.append(randomNum2)
+
+print(randomNum)
