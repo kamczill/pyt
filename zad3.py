9085413ac8ff5a5d382d62dfd0536de1e41f4367
kamczill <kam134@interia.eu>
2020-11-24 14:39:05 +0100

    finalna wersja

--- zad3.py
+++ zad3.py
@@ -4,8 +4,20 @@ counter = 0
 passwordIn = ''
 
 while passwordIn != password:
-    tries -= counter
-    print('Masz jeszcze %d prob' % (tries))
+    print('\n\nIlosc prób: %d' % (tries-counter))
     passwordIn = input('Podaj haslo: ')
-
+    counter += 1
     if passwordIn == password:
+        win = True
+        break
+    elif counter == tries:
+        win = False
+        break
+    else:
+        print('\nHaslo nieprawidlowe')
+
+
+if counter == tries:
+    print('\nNie udało Ci sie zgadnąć hasła ;c')
+else:
+    print('\nGRATULACJE!!! Hasło to %s. Udało Ci się zgadnąć w %d próbach' % (password, counter))
