33c320a84c8200e3abcfae8d7273decf35d8f52e
kamczill <kam134@interia.eu>
2020-11-24 15:05:28 +0100

    2 pomysly

--- zad4.py
+++ zad4.py
@@ -1,18 +1,24 @@
 from random import randint
-import time
+from time import perf_counter_ns
 
 long_list = [randint(0, 3000) for element in range(1000000)]
 
-start_time = time.clock()
+start = perf_counter_ns()
+
+#pomysl 1
 for element in long_list:
     if element == -1:
         print("Element -1 należy do listy")
         break
-print (time.clock() - start_time, "seconds")
+
+
+stop = perf_counter_ns()
+print ("Pierwsza operacja trwała ", stop-start, "nanoseconds\n")
 
 i=0
 number = 0
 
+start = perf_counter_ns()
 while number != -1:
     number = long_list[i]
     if number == -1:
@@ -22,4 +28,5 @@ while number != -1:
         print("nie ma tej liczby w tej liście")
         break
     i += 1
-
+stop = perf_counter_ns()
+print ("Druga operacja trwała ", stop-start, "nanoseconds")
